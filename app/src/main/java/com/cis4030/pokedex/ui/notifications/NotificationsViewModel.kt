a9a73df9d3e7439629765fce0d8b837a95d99f8e
Jacob Michels <jacob.michels2025@gmail.com>
2021-03-02 18:31:15 -0500

    Initial commit

---
+++ app/src/main/java/com/cis4030/pokedex/ui/notifications/NotificationsViewModel.kt
@@ -0,0 +1,13 @@
+package com.cis4030.pokedex.ui.notifications
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class NotificationsViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is notifications Fragment"
+    }
+    val text: LiveData<String> = _text
+}
