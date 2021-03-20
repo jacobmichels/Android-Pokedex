94be82014f3e3598902cfc30144a7fb9233c2125
Jacob Michels <jacob.michels2025@gmail.com>
2021-03-20 03:38:13 -0400

    query pokeAPI on init and display total number of pokemon received

--- app/src/main/java/com/cis4030/pokedex/ui/pokedex_list/HomeViewModel.kt
+++ app/src/main/java/com/cis4030/pokedex/ui/pokedex_list/HomeViewModel.kt
@@ -3,6 +3,10 @@ package com.cis4030.pokedex.ui.pokedex_list
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.cis4030.pokedex.network.PokeAPINetwork
+import kotlinx.coroutines.launch
+import kotlin.coroutines.coroutineContext
 
 class HomeViewModel : ViewModel() {
 
@@ -10,4 +14,12 @@ class HomeViewModel : ViewModel() {
         value = "This is where the pokemon entries will appear."
     }
     val text: LiveData<String> = _text
+
+    init{
+        viewModelScope.launch {
+            val list = PokeAPINetwork.pokeAPI.getPokemon()
+            _text.value = list.count.toString()
+        }
+
+    }
 }
