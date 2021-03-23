520bf4b5b01fdfab9842a2eb84ba60655f23ffd7
Jacob Michels <jacob.michels2025@gmail.com>
2021-03-23 16:16:09 -0400

    refactor to include only required typeconverters

--- app/src/main/java/com/cis4030/pokedex/database/ListTypeConverters.kt
+++ app/src/main/java/com/cis4030/pokedex/database/ListTypeConverters.kt
@@ -2,6 +2,7 @@ package com.cis4030.pokedex.database
 
 import androidx.room.TypeConverter
 import com.cis4030.pokedex.domain.Ability
+import com.cis4030.pokedex.domain.Effect
 import com.cis4030.pokedex.domain.Move
 import com.cis4030.pokedex.domain.Type
 import com.squareup.moshi.Moshi
@@ -10,40 +11,29 @@ import com.squareup.moshi.Types
 class ListTypeConverters {
 
     private val moshi: Moshi = Moshi.Builder().build()
-    private val listOfMovesType = Types.newParameterizedType(List::class.java,Move::class.java)
-    private val listOfMovesAdapter = moshi.adapter<List<Move>>(listOfMovesType)
 
-    private val listOfAbilitiesType = Types.newParameterizedType(List::class.java,Ability::class.java)
-    private val listOfAbilitiesAdapter = moshi.adapter<List<Ability>>(listOfAbilitiesType)
+    private val listOfStringsType = Types.newParameterizedType(List::class.java,String::class.java)
+    private val listOfStringsAdapter = moshi.adapter<List<String>>(listOfStringsType)
 
-    private val listOfTypesType = Types.newParameterizedType(List::class.java, Type::class.java)
-    private val listOfTypesAdapter = moshi.adapter<List<Type>>(listOfTypesType)
+    private val listOfEffectsType = Types.newParameterizedType(List::class.java, Effect::class.java)
+    private val listOfEffectsAdapter = moshi.adapter<List<Effect>>(listOfEffectsType)
 
     @TypeConverter
-    fun moveListToJson(moves:List<Move>?):String?{
-        return listOfMovesAdapter.toJson(moves)
+    fun stringListToJson(strings: List<String>?):String?{
+        return listOfStringsAdapter.toJson(strings)
+    }
+    @TypeConverter
+    fun jsonToStringList(string: String?):List<String>?{
+        return string?.let { listOfStringsAdapter.fromJson(string) }
     }
 
     @TypeConverter
-    fun jsonToMoveList(string: String?): List<Move>? {
-        return string?.let{listOfMovesAdapter.fromJson(string)}
+    fun effectListToJson(effects: List<Effect>?):String?{
+        return listOfEffectsAdapter.toJson(effects)
     }
 
     @TypeConverter
-    fun abilityListToJson(abilities:List<Ability>?):String?{
-        return listOfAbilitiesAdapter.toJson(abilities)
-    }
-    @TypeConverter
-    fun jsonToAbilityList(string: String?):List<Ability>?{
-        return string?.let{listOfAbilitiesAdapter.fromJson(string)}
-    }
-
-    @TypeConverter
-    fun typeListToJson(types:List<Type>?):String?{
-        return listOfTypesAdapter.toJson(types)
-    }
-    @TypeConverter
-    fun jsonToTypeList(string: String?):List<Type>?{
-        return string?.let{listOfTypesAdapter.fromJson(string)}
+    fun jsonToEffectList(string:String?):List<Effect>?{
+        return string?.let{ listOfEffectsAdapter.fromJson(string)}
     }
 }
