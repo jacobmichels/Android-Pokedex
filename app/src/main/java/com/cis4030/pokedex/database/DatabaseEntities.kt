10b4de48e920194943417f542ca0b6f2a7e362ce
Jacob Michels <jacob.michels2025@gmail.com>
2021-03-23 16:04:22 -0400

    add completed database entities

--- app/src/main/java/com/cis4030/pokedex/database/DatabaseEntities.kt
+++ app/src/main/java/com/cis4030/pokedex/database/DatabaseEntities.kt
@@ -1,27 +1,68 @@
 package com.cis4030.pokedex.database
 
+import android.util.Log
+import androidx.room.Database
 import androidx.room.Entity
 import androidx.room.PrimaryKey
-import com.cis4030.pokedex.domain.Ability
-import com.cis4030.pokedex.domain.Move
-import com.cis4030.pokedex.domain.Type
+import com.cis4030.pokedex.domain.*
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 
 @Entity
 data class DatabasePokemon constructor(
     @PrimaryKey
-    val id: Int,
     val name: String,
+    val id: Int,
     val height: Int,
     val weight: Int,
-    val imageUrl:String,
+    val imageUrl: String,
     val baseExperience: Int,
-    val abilities: List<Ability>,
+    val abilities: List<String>,
     val baseHP: Int,
     val baseAtk: Int,
     val baseDfn: Int,
     val spAtk: Int,
     val spDfn: Int,
     val speed: Int,
-    val types: List<Type>,
-    val possibleMoves: List<Move>
+    val types: List<String>,
+    val possibleMoves: List<String>,
+    val custom: Boolean
+)
+
+@Entity
+data class DatabaseType constructor(
+    @PrimaryKey
+    val name: String,
+    val moveList: List<String>,
+    val pokemonList: List<String>,
+    val doubleDamageFrom: List<String>,
+    val doubleDamageTo: List<String>,
+    val halfDamageFrom: List<String>,
+    val halfDamageTo: List<String>,
+    val noDamageFrom: List<String>,
+    val noDamageTo: List<String>
+)
+
+@Entity
+data class DatabaseAbility constructor(
+    @PrimaryKey
+    val name: String,
+    val description: String,
+    val shortDescription:String,
+    val pokemonList: List<String>
+)
+
+@Entity
+data class DatabaseMove constructor(
+    @PrimaryKey
+    val name: String,
+    val type: String,
+    val accuracy: Int?,
+    val damageType: String?,
+    val effects: List<Effect>,
+    val learnedByPokemon: List<String>,
+    val power: Int?,
+    val pp: Int?,
+    val priority: Int,
+    val target: String
 )
