9c4583a88556b99f13e095c52fd40359fb7b10d8
Jacob Michels <jacob.michels2025@gmail.com>
2021-03-28 04:12:37 -0400

    add assets and dialog skeleton for sorting/filtering pokemon

--- app/src/main/java/com/cis4030/pokedex/ui/pokedex_list/PokedexListFragment.kt
+++ app/src/main/java/com/cis4030/pokedex/ui/pokedex_list/PokedexListFragment.kt
@@ -4,9 +4,11 @@ import android.os.Bundle
 import android.util.Log
 import android.view.*
 import android.widget.TextView
+import android.widget.Toast
 import androidx.annotation.LayoutRes
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.activityViewModels
 import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.RecyclerView
@@ -48,6 +50,7 @@ class PokedexListFragment : Fragment() {
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         return when(item.itemId){
             R.id.refresh_menu_button ->{
+                Toast.makeText(context,"Refreshing information. This may take a few minutes.",Toast.LENGTH_LONG).show()
                 viewModel.refreshDatabase()
                 true
             }
@@ -57,6 +60,11 @@ class PokedexListFragment : Fragment() {
             }
             R.id.settings_menu_button ->{
                 Log.d("POKEDEX","Settings menu button clicked.")
+                true
+            }
+            R.id.filter_sort_menu_button -> {
+                Log.d("POKEDEX","Filter menu button clicked.")
+                val dialogFragment = SortFilterDialogFragment().show(parentFragmentManager,"Dialog")
                 true
             }
             else -> super.onOptionsItemSelected(item)
